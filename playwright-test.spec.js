1b23518fa44efff5ac52740444566c768d6e3572
lafafor <69935722+lafafor@users.noreply.github.com>
2026-04-09 00:18:46 +0300

    Update playwright-test.spec.js

--- playwright-test.spec.js
+++ playwright-test.spec.js
@@ -1,14 +1,6 @@
-const { test, expect } = require('@playwright/test');
-
-test('Проверка заголовка страницы Playwright', async ({ page }) => {
-  // Открыть страницу
-  await page.goto('https://playwright.dev/');
-  
-  // Получить заголовок страницы
-  const title = await page.title();
-  
-  // Проверить, что заголовок содержит "Playwright"
-  expect(title).toContain('Playwright');
-  
- 
-});
+module.exports = {
+  projects: [
+    { name: 'Chromium', use: { browserName: 'chromium' } },
+    { name: 'Firefox', use: { browserName: 'firefox' } },
+  ],
+};
